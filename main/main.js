30f13cba651322e939670dbca7f681ffbed93aa1
Elliot Huang <Huang_E@outlook.com>
2022-11-26 15:56:20 -0500

    main UI

---
+++ main/main.js
@@ -0,0 +1,12 @@
+// var map = d3.select('#map');
+// var mapWidth = +map.attr('width');
+// var mapHeight = +map.attr('height');
+
+
+// var myMap = L.map('map').setView([37.8, -96], 4);
+
+// L.tileLayer(
+//     'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>',
+//     maxZoom: 6,
+//     }).addTo(myMap);
