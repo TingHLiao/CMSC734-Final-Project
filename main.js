68f3d78a3e4fdcfe13d04e7eb6b04bd197d23390
yaochih <yaochihlee@gmail.com>
2022-11-27 15:33:08 -0500

    add slider modes

--- main.js
+++ main.js
@@ -9,20 +9,21 @@ var filtered_dataset;
 var filter_min_date;
 var filter_max_date;
 var date_interpolate_func;
-var display_date_format = d3.timeFormat('%m/%d/%Y');
-function sliderSlidingListener(handler, value) {
-    /*if(handler == 0)
-        filter_min_date = structuredClone(date_interpolate_func(value));
-    else
-        filter_max_date = structuredClone(date_interpolate_func(value));
-    draw_map();*/
-    return display_date_format(date_interpolate_func(value));
+var date_format = d3.timeFormat('%Y/%m/%d');
+
+function sliderSlidingListener(values) {
+    updateDateRange(values);
+
+    return [date_format(date_interpolate_func(values[0])), 
+            date_format(date_interpolate_func(values[1]))];
 }
-function sliderStopListener(value0, value1) {
-    filter_min_date = structuredClone(date_interpolate_func(value0));
-    filter_max_date = structuredClone(date_interpolate_func(value1));
-    
-    update(dataset);
+
+function updateDateRange(values) {
+    console.log(values);
+    filter_min_date = structuredClone(date_format(date_interpolate_func(values[0])));
+    filter_max_date = structuredClone(date_format(date_interpolate_func(values[1])));
+    console.log(filter_max_date, filter_min_date);
+    update();
 }
 
 // initialize dataset and two views
@@ -32,17 +33,13 @@ Promise.all([
 ]).then(function (data) {
     dataset = data[0];
     state_coords = data[1];
-    //load_map_data(state_coords);
 
     // time format 
     var time_parse = d3.timeParse('%Y/%m/%d');
-    dataset.forEach(function(d, i) {
-        dataset[i].date = time_parse(d.date);
-    });
-    filter_min_date = time_parse('2020/01/22'); //d3.min(dataset, function(d) {return d.date;});
+    filter_min_date = '2020/01/22'; //d3.min(dataset, function(d) {return d.date;});
     filter_max_date = d3.max(dataset, function(d) {return d.date;});
-    date_interpolate_func = d3.interpolateDate(filter_min_date, filter_max_date);
-
+    date_interpolate_func = d3.interpolateDate(time_parse(filter_min_date), time_parse(filter_max_date));
+    changeSliderMode("time");
     update();
 });
 
@@ -51,7 +48,6 @@ function update() {
     filtered_dataset = dataset.filter(function(d, i) {
         return d.date >= filter_min_date && d.date <= filter_max_date;
     });
-
     show_map(show_attr);
     show_secondary_view();
 }
--- utils/slider.js
+++ utils/slider.js
@@ -1,21 +1,72 @@
+var single_value = 0.5;
+var range_values = [0, 1];
 $("#slider-range").slider({
-    range: true,
-    orientation: "horizontal",
-    min: 0,
-    max: 1,
-    values: [0, 1],
-    step: 0.001,
-  
-    slide: function (event, ui) {
-      if (ui.values[0] == ui.values[1]) {
-        return false;
-      }
-      
-      $("#slider_min").text(sliderSlidingListener(0, ui.values[0]));
-      $("#slider_max").text(sliderSlidingListener(1, ui.values[1]));
-    },
+  range: true,
+  orientation: "horizontal",
+  min: 0,
+  max: 1,
+  values: [0, 1],
+  step: 0.001,
 
-    stop: function(event, ui) {
-        sliderStopListener(ui.values[0], ui.values[1]);
+  slide: function (event, ui) {
+    if (ui.values[0] == ui.values[1]) {
+      return false;
     }
-  });
+    range_values = ui.values;
+    update_slider_tag(range_values);
+  },
+
+  stop: function(event, ui) {
+  }
+});
+
+$("#slider-single").slider({
+  range: false,
+  orientation: "horizontal",
+  min: 0,
+  max: 1,
+  value: single_value,
+  step: 0.001,
+
+  slide: function (event, ui) {
+    single_value = ui.value;
+    update_slider_tag(single_value);
+  },
+
+  stop: function(event, ui) {
+      //sliderStopListener(ui.value);
+  }
+});
+
+
+var slider_mode = 'time';
+$('#slider-range').hide();
+function changeSliderMode(mode) {
+  slider_mode = mode;
+  if(mode == 'time') {
+    $('#slider-single').show();
+    $('#slider-range').hide();
+    updateDateRange(single_value);
+  } else {
+    $('#slider-single').hide();
+    $('#slider-range').show();
+    updateDateRange(range_values);
+  }
+  update_slider_tag();
+  
+}
+
+function update_slider_tag() {
+  if(slider_mode == 'time') {
+    $("#slider-value1").css('left', "" + 100*single_value + "%");
+    $("#slider-value2").hide();
+    $("#slider-value1").text(sliderSlidingListener([single_value, single_value])[0]);
+  } else {
+    $("#slider-value2").show();
+    $("#slider-value1").css('left', "" + 100*range_values[0] + "%");
+    $("#slider-value2").css('left', "" + 100*range_values[1] + "%");
+    dates = sliderSlidingListener(range_values);
+    $("#slider-value1").text(dates[0]);
+    $("#slider-value2").text(dates[1]);
+  }
+}
